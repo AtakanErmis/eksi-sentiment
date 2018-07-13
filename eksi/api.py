0864d4bb8b967be6e98e3d5c74ddde3d975b0781
Toprak Öztürk <toprakozt@gmail.com>
2018-07-13 03:43:26 +0300

    Fix error in xpath

--- eksi/api.py
+++ eksi/api.py
@@ -11,26 +11,34 @@ EKSI_ENTRY_URL = EKSI_BASE_URL + "entry/"
 
 ENTRY_BODY_XPATH = r'//*[@id="entry-item-list"]/li/div[1]'
 ENTRY_FAV_XPATH = r'//*[@id="entry-item-list"]/li/footer/div[1]/span[3]/a[2]'
-ENTRY_AUTHOR_XPATH = r'//*[@id="entry-item-list"]/li/footer/div[2]/a[2]'
-ENTRY_DATE_XPATH = r'//*[@id="entry-item-list"]/li/footer/div[2]/a[1]'
+ENTRY_AUTHOR_XPATH = r'//a[@class="entry-author"]'
+ENTRY_DATE_XPATH = r'//a[@class="entry-date permalink"]'
 
-
-def _normalize_date(self, date: str) -> str:
+def _normalize_date(date: str) -> str:
     """
-    Remove the modification date and split the datetime format into 
-    seperate date and time format.
+    Modifiye edilme tarihini sil ve string olan tarih-zaman verisini datetime 
+    objesine cevir.
+
     Args:
-        date: Raw date time string
-    Returns:
+        date: Ciplak tarih zaman stringi
+    Return:
         tuple: Original date and time.
-    Example:
+    Ornek:
         '18.05.2018 17:50 ~ 18:30' -> datetime(2018, 5, 18, 17, 50)
     """
     original_date = date.split(' ~ ')[0]
-    return datetime.strptime(original_date, '%d.%m.%Y %H:%M')
+
+    # sozluk ilk kuruldugunda entry tarihlerindeki saat tutulmazmis.
+    # eger saat yazarken string'in icinde bulunan : varsa saatle beraber 
+    # yazdiriyoruz. Yoksa 00:00 olarak yazdiriyoruz.
+    if ':' in original_date:
+        # TODO: bunun daha iyi bir yolu vardir
+        return datetime.strptime(original_date, '%d.%m.%Y %H:%M')
+    else:
+        return datetime.strptime(original_date + ' 00:00', '%d.%m.%Y %H:%M')
 
 
-def _is_entry_available(self, page_content: str) -> bool:
+def _is_entry_available(page_content: str) -> bool:
     """Return false if the entry has been deleted."""
     if 'data-not-found="true"' in page_content:
         return False
@@ -54,11 +62,13 @@ def get_entry_by_id(entry_id):
     response = requests.get(EKSI_ENTRY_URL + str(entry_id))
     page_content = response.content.decode('utf-8')
 
-    # pass if the entry has been 404'ed
-    if _is_entry_available(content):
+    with open('content.html', 'w') as file:
+        print(page_content, file=file)
+
+    if not _is_entry_available(page_content):
         return None
     
-    tree = html.fromstring(content)
+    tree = html.fromstring(page_content)
     owner = tree.xpath(ENTRY_AUTHOR_XPATH + '/text()')[0]
     body_elems = []
     for elem in tree.xpath(ENTRY_BODY_XPATH + '//text()'):
@@ -83,7 +93,3 @@ def get_entry_by_id(entry_id):
     }
 
     return entry    
-
-if __name__ == "__main__":
-    x = get_entry_by_id(1)
-    print(x['body'])
