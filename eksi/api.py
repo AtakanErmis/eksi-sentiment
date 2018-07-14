3291ed5135918fb553ff9e38bd8c1540598a864a
Toprak Öztürk <toprakozt@gmail.com>
2018-07-14 21:22:21 +0300

    Basliga gore entry alma eklendi

--- eksi/api.py
+++ eksi/api.py
@@ -14,6 +14,9 @@ ENTRY_FAV_XPATH = r'//*[@id="entry-item-list"]/li/footer/div[1]/span[3]/a[2]'
 ENTRY_AUTHOR_XPATH = r'//a[@class="entry-author"]'
 ENTRY_DATE_XPATH = r'//a[@class="entry-date permalink"]'
 
+TOPIC_ITEM_LIST_XPATH = r'//li[@data-author]'
+TOPIC_NEXT_PAGE_XPATH = r'//a[@class="next"]'
+
 
 def _normalize_date(date: str) -> str:
     """
@@ -30,7 +33,7 @@ def _normalize_date(date: str) -> str:
     original_date = date.split(' ~ ')[0]
 
     # sozluk ilk kuruldugunda entry tarihlerindeki saat tutulmazmis.
-    # eger saat yazarken string'in icinde bulunan : varsa saatle beraber 
+    # eger saat yazarken string'in icinde bulunan : varsa saatle beraber
     # yazdiriyoruz. Yoksa 00:00 olarak yazdiriyoruz.
     if ':' in original_date:
         # TODO: bunun daha iyi bir yolu vardir
@@ -69,7 +72,7 @@ def get_entry_by_id(entry_id: int) -> dict:
 
     if not _is_entry_available(page_content):
         return None
-    
+
     tree = html.fromstring(page_content)
     owner = tree.xpath(ENTRY_AUTHOR_XPATH + '/text()')[0]
     body_elems = []
@@ -85,7 +88,7 @@ def get_entry_by_id(entry_id: int) -> dict:
         # this element does not exist if the favcount of the entry is 0
         # and so it raises IndexError
         fav = 0
-    
+
     entry = {
         'id': entry_id,
         'owner': owner,
@@ -94,4 +97,43 @@ def get_entry_by_id(entry_id: int) -> dict:
         'fav': fav
     }
 
-    return entry    
+    return entry
+
+
+def get_entries_by_topic(topic: str) -> list:
+    entries = []
+    topic_url = requests.get(EKSI_BASE_URL + f"?q={topic}").url
+
+    page = 1
+    while True:
+        url = topic_url + f"?p={page}"
+        response = requests.get(url)
+        page_content = response.content.decode('utf-8')
+
+        if 'data-not-found="true"' in page_content:
+            break
+        
+        tree = html.fromstring(page_content)
+        elements = tree.xpath(TOPIC_ITEM_LIST_XPATH)
+
+        for elem in elements:
+            date = elem.xpath(r'//a[@class="entry-date permalink"]//text()')[0]
+            body = ' '.join(elem.xpath(r'//div[@class="content"]//text()'))
+            entry = {
+                'id': int(elem.get("data-id")),
+                'owner': elem.get("data-author"),
+                'body': body,
+                'date': _normalize_date(date),
+                'fav': int(elem.get("data-favorite-count"))
+            }
+            entries.append(entry)
+        
+        page += 1
+
+    return entries
+
+
+def get_entries_by_profile(profile_slug: str) -> list():
+    pass
+
+# TODO: Link verilip ona gore baslik alma da yapilacak.
