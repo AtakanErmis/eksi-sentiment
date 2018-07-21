383af98b37cc7d1495f57114debc819c7cab6839
Toprak Öztürk <toprakozt@gmail.com>
2018-07-22 01:08:17 +0300

    Preprocesste duzeltme

--- eksi/preprocess.py
+++ eksi/preprocess.py
@@ -60,10 +60,7 @@ def remove_numeric(string: str) -> str:
         >>> remove_numeric("2018de cikan iphone 10 cok guzel.")
         "de cikan iphone cok guzel."
     """
-    numeric = re.findall("[0-9]", string)
-    for i in numeric:
-        string = string.replace(i, '')
-    return string
+    return _trim_spaces(re.sub("[0-9]", '', string))
 
 
 def remove_links(string: str) -> str:
@@ -75,11 +72,12 @@ def remove_links(string: str) -> str:
         >>> remove_links("google diye bir sey cikmis: http://google.com/")
         "google diye bir sey cikmis:"
     """
-    # silindikten sonra aralarda fazla boşluk olabiliyor
-    links = re.findall("(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?", string)
-    for i in links:
-        string = string.replace(f"{i[0]}://{i[1]}{i[2]}", '')
-    return string
+    return _trim_spaces(
+        re.sub(r"(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))"
+               r"([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?",
+               '',
+               string)
+    )
 
 
 def stem(string: str) -> str:
